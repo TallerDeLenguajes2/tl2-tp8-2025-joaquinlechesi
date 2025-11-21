bdf302eb61e3d8e0ec5dd6471afb41ce79b0d5b1
joaquinlechesi <joaquinlechesi@gmail.com>
2025-11-21 19:16:15 -0300

    Se agregaron atribustos a AgregarProductosViewModel

--- ViewModels/AgregarProductoViewModel.cs
+++ ViewModels/AgregarProductoViewModel.cs
@@ -5,10 +5,12 @@ namespace MiWebAPI.ViewModels.AgregarProductoViewModel
 {
     public class AgregarProductoViewModel
     {
-        [Required(ErrorMessage = "La descripcion es obligatoria.")]
+        //[Required(ErrorMessage = "La descripcion es obligatoria.")]
+        [StringLength(250)]
         public string Description { get; set; }
 
         [Required(ErrorMessage = "El precio es obligatorio")]
+        [Range(0,100000)]
         public int Precio { get; set; }
     }
 }
