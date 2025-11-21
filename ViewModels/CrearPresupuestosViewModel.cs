ab983acfd7d6456b6f708f40bcf1e1412b74a200
joaquinlechesi <joaquinlechesi@gmail.com>
2025-11-21 19:13:21 -0300

    CrearPresupuestosViewModel.cs creado para el alta de Presupuestos con casi todas las validaciones (falta una de fecha)

---
+++ ViewModels/CrearPresupuestosViewModel.cs
@@ -0,0 +1,27 @@
+using System.ComponentModel.DataAnnotations;
+using MiWebAPI.Models;
+
+namespace MiWebAPI.ViewModels.PresupuestosViewModel
+{
+    public class CrearPresupuestosViewModel
+    {
+        // [Required]
+        // public int IdPresupuestos { get; set; }
+        [Required(ErrorMessage = "Debe ingresar un nombre")]
+        public string NombreDestinatario { get; set; }
+        [EmailAddress(ErrorMessage = "Debe tener formato de correo electronico")]
+        public string Correo { get; set; }
+        [Required][DataType(DataType.Date)] //Falta controlar que la fecha sea menor a la actual
+        public DateTime FechaCreacion { get; set; }
+        // public PresupuestosViewModel()
+        // {
+            
+        // }
+        // public PresupuestosViewModel(Presupuestos presupuesto)
+        // {
+        //     //IdPresupuestos = presupuesto.IdPresupuestos;
+        //     NombreDestinatario = presupuesto.NombreDestinatario;
+        //     FechaCreacion = presupuesto.FechaCreacion;
+        // }
+    }
+}
