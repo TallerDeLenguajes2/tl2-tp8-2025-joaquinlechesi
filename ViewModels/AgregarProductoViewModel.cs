ef908d4584073b771cd4cd56a7377c64f6c982e1
joaquinlechesi <joaquinlechesi@gmail.com>
2025-11-25 01:43:01 -0300

    Se agregaron modificaciones a AgregarProductoViewModel

--- ViewModels/AgregarProductoViewModel.cs
+++ ViewModels/AgregarProductoViewModel.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
 using MiWebAPI.Models;
 using System.ComponentModel.DataAnnotations;
 
@@ -5,7 +6,22 @@ namespace MiWebAPI.ViewModels.AgregarProductoViewModel
 {
     public class AgregarProductoViewModel
     {
-        [Required][Range(0,100)] //De manera provisoria dejamos como rango maximo 100
+        //[Required]
+        public int IdPresupuestos { get; set; }
+
+        [Display(Name = "Producto a agregar")]
+        public int IdProducto { get; set; }
+
+        [Display(Name = "Cantidad")]
+        [Required(ErrorMessage = "La cantidad es obligatoria")]
+        [Range(0, int.MaxValue, ErrorMessage = "La cantidad debe ser mayor a cero.")] //De manera provisoria dejamos como rango maximo 100
         public int Cantidad { get; set; }
+
+        public SelectList ListaProductos { get; set; }
+
+        // public AgregarProductoViewModel() //Dejo el constructor por precaucion
+        // {
+            
+        // }
     }
 }
