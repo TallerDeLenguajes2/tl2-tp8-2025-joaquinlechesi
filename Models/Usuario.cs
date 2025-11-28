e69392e41fb12314879673d61a44eabc41bc406b
joaquinlechesi <joaquinlechesi@gmail.com>
2025-11-28 05:40:29 -0300

    Se creo el modelo Usuario

---
+++ Models/Usuario.cs
@@ -0,0 +1,16 @@
+namespace MiWebAPI.Models;
+
+public class Usuario
+{
+    public int Id { get; set; }
+    public string Nombre { get; set; }
+    public string User { get; set; }
+    public string Pass { get; set; }
+    public string Rol { get; set; } //enumerable??
+}
+
+public enum Rol
+{
+    Administrador,
+    Cliente
+}
