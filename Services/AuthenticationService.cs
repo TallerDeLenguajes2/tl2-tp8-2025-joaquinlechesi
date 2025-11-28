f84ef07bc2495da91c63b6dca37348258cef0d59
joaquinlechesi <joaquinlechesi@gmail.com>
2025-11-28 14:41:32 -0300

    Se agrego la logica de Login y la logica de Chequeo cada uno con sus respectivos metodos (Login(), IsAuthenticated() y HasAccessLevel(rol))

--- Services/AuthenticationService.cs
+++ Services/AuthenticationService.cs
@@ -1,13 +1,67 @@
+using Microsoft.AspNetCore.Identity.Data;
 using MiWebAPI.Interfaces;
-namespace MiWebAPI.Services;
-
-public class AuthenticationService : IAuthenticationService
+using MiWebAPI.Repository;
+namespace MiWebAPI.Services
 {
-    private readonly IUserRepository _userRepository;
-    private readonly IHttpContextAccessor _httpContextAccessor;
-    public AuthenticationService(IUserRepository userRepository, IHttpContextAccessor httpContextAccessor)
+    public class AuthenticationService : IAuthenticationService
     {
-        _userRepository = userRepository;
-        _httpContextAccessor = httpContextAccessor;
+        private readonly IUserRepository _userRepository;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        //private readonly HttpContext context;
+        public AuthenticationService(IUserRepository userRepository, IHttpContextAccessor httpContextAccessor)
+        {
+            _userRepository = userRepository;
+            _httpContextAccessor = httpContextAccessor;
+            // context = _httpContextAccessor.HttpContext;
+        }
+        public bool Login(string username, string password)
+        {
+            var context = _httpContextAccessor.HttpContext;
+            var user = _userRepository.GetUser(username,password);
+            if (user != null)
+            {
+                if (context == null)
+                {
+                    throw new InvalidOperationException("HttpContext no está disponible.");
+                }
+                context.Session.SetString("IsAuthenticated", "true");
+                context.Session.SetString("User", user.User);
+                context.Session.SetString("UserNombre", user.Nombre);
+                context.Session.SetString("Rol", user.Rol); //es el tipo de acceso/rol admin o cliente
+                return true;
+            }
+            return false;
+        }
+        public void Logout()
+        {
+            var context = _httpContextAccessor.HttpContext;
+            if (context == null)
+            {
+                throw new InvalidOperationException("HttpContext no está disponible.");
+            }
+            /* context.Session.Remove("IsAuthenticated");
+            context.Session.Remove("User");
+            context.Session.Remove("UserNombre");
+            context.Session.Remove("Rol"); */
+            context.Session.Clear();
+        }
+        public bool IsAuthenticated()
+        {
+            var context = _httpContextAccessor.HttpContext;
+            if (context == null)
+            {
+                throw new InvalidOperationException("HttpContext no está disponible.");
+            }
+            return context.Session.GetString("IsAuthenticated") == "true";
+        }
+        public bool HasAccessLevel(string requiredAccessLevel)
+        {
+            var context = _httpContextAccessor.HttpContext;
+            if (context == null)
+            {
+                throw new InvalidOperationException("HttpContext no está disponible.");
+            }
+            return context.Session.GetString("Rol") == requiredAccessLevel;
+        }
     }
 }
