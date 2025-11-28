66d7033f0d5aabf9e2c0670c656ec33271cfc77d
joaquinlechesi <joaquinlechesi@gmail.com>
2025-11-28 05:13:09 -0300

    Interfas de usuario creadad

---
+++ Interfaces/IUserRepository.cs
@@ -0,0 +1,8 @@
+namespace MiWebAPI.Interfaces
+{
+    public interface IUserRepository
+    {
+        // Retorna el objeto Usuario si las credenciales son válidas, sino null.
+        // Usuario GetUser(string usernamem, string password)
+    }
+}
