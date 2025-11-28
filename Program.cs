40b8bea2ae4a43972922de886ccfbd2d589f4b61
joaquinlechesi <joaquinlechesi@gmail.com>
2025-11-28 13:16:24 -0300

    Se agregaron los using, registros y pipeline de sesion en Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,14 @@
+using Microsoft.AspNetCore.Http;
+using MiWebAPI.Interfaces;
+using MiWebAPI.Repository.ProductoRepository;
+using MiWebAPI.Services;
 var builder = WebApplication.CreateBuilder(args);
+
+// Registro de la Inyección de Dependencia (TODOS AddScoped)
+builder.Services.AddScoped<IProductoRepository, ProductoRepository>();
+builder.Services.AddScoped<IPresupuestoRepository, PresupuestoRepository>();
+//builder.Services.AddScoped<IUserRepository, UsuarioRepository>();
+builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -12,11 +22,14 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+app.UseSession();
 
 app.UseHttpsRedirection();
+app.UseStaticFiles();
 app.UseRouting();
-
 app.UseAuthorization();
+
+
 
 app.MapStaticAssets();
 
@@ -25,5 +38,13 @@ app.MapControllerRoute(
     pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
 
+builder.Services.AddHttpContextAccessor();
+// Habilitar servicios de sesiones
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(30); // Tiempo de expiración de la sesión
+    options.Cookie.HttpOnly = true; // Solo accesible desde HTTP, no JavaScript
+    options.Cookie.IsEssential = true; // Necesario incluso si el usuario no acepta cookies
+});
 
 app.Run();
