14bacaaa9aa3d1acc5063475f6bf51f268ad19a6
joaquinlechesi <joaquinlechesi@gmail.com>
2025-11-28 05:55:28 -0300

    Se creo UsusarioRepository.cs y se implemento la interfas IUserRepository

--- Interfaces/IUserRepository.cs
+++ Interfaces/IUserRepository.cs
@@ -1,8 +1,9 @@
+using MiWebAPI.Models;
 namespace MiWebAPI.Interfaces
 {
     public interface IUserRepository
     {
         // Retorna el objeto Usuario si las credenciales son válidas, sino null.
-        // Usuario GetUser(string usernamem, string password)
+        Usuario GetUser(string usernamem, string password);
     }
 }
---
+++ Repository/UsuarioRepository.cs
@@ -0,0 +1,40 @@
+using MiWebAPI.Interfaces;
+using MiWebAPI.Models;
+using Microsoft.Data.Sqlite;
+namespace MiWebAPI.Repository;
+
+public class UsuarioRepository : IUserRepository
+{
+    private string cadenaConexion = "Data source = Db/Tienda.db";
+    public Usuario GetUser(string username, string password)
+    {
+        Usuario user = null;
+
+        //Consulta SQL que busca por Usuario Y Contrasena
+        const string sql = @"
+                            SELECT Id, Nombre, User, Pass, Rol
+                            FROM Usuarios
+                            WHERE User = @Usuario AND Pass = @Contrasena";
+        using var conexion = new SqliteConnection(cadenaConexion);
+        conexion.Open();
+        using var comando = new SqliteCommand(sql, conexion);
+        // Se usan parámetros para prevenir inyección SQL
+        comando.Parameters.AddWithValue("@Usuario", username);
+        comando.Parameters.AddWithValue("@Contrasena", password);
+        using var reader = comando.ExecuteReader();
+        if (reader.Read())
+        {
+            // Si el lector encuentra una fila, el usuario existe y las credenciales son correctas
+            user = new Usuario
+            {
+                Id = reader.GetInt32(0),
+                Nombre = reader.GetString(1),
+                User = reader.GetString(2),
+                Pass = reader.GetString(3),
+                Rol = reader.GetString(4)
+            };
+        }
+
+        return user;
+    }
+}
